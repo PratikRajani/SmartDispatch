ad33d0471e7365e2e5e1a790623d3054d83b33ec
PratikRajani <pratikrajani98@gmail.com>
2019-04-18 13:18:17 +0530

    About Us

--- app/src/main/java/pratik/smartdispatch/MainActivity.java
+++ app/src/main/java/pratik/smartdispatch/MainActivity.java
@@ -1,26 +1,40 @@
 package pratik.smartdispatch;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn, loginbtn, vehicleloginbtn, adminloginbtn, hospitalloginbtn;
+    private Button btn, loginbtn, vehicleloginbtn, adminloginbtn, hospitalloginbtn, aboutus;
     private TextView textView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginbtn = (Button) findViewById(R.id.loginbtn);
-        vehicleloginbtn = (Button) findViewById(R.id.vehicleloginbtn);
-        hospitalloginbtn = (Button) findViewById(R.id.hospitalloginbtn);
-        adminloginbtn = (Button) findViewById(R.id.adminloginbtn);
+        loginbtn = (Button) findViewById(R.id.user_login_button);
+        vehicleloginbtn = (Button) findViewById(R.id.vehicle_login_button);
+        hospitalloginbtn = (Button) findViewById(R.id.hospital_login_button);
+        adminloginbtn = (Button) findViewById(R.id.admin_login_button);
+        aboutus = (Button) findViewById( R.id.about_us );
+
+        aboutus.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AboutUs aboutUs = new AboutUs();
+                aboutUs.show( getSupportFragmentManager(), "About Us" );
+            }
+        } );
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("loginType", user_login);
         startActivity(intent);
     }
+
 }
 
 
